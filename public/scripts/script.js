5ddd2677d1d7d570f312e84dae5779497c293d81
Piotr Kedzierski <piotr.kedzierski.88@gmail.com>
2020-11-26 16:26:31 +0100

    add more styling and UI fixes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,21 @@
-$('#summernote').summernote({
-    height: 300
+$(".summernote").summernote({
+    height: 250,
+    toolbar: [
+      ['style', ['bold', 'italic', 'underline', 'clear']],
+      ['font', ['strikethrough', 'superscript', 'subscript']],
+      ['fontname', ['fontname']],
+      ['fontsize', ['fontsize']],
+      ['color', ['color']],
+      ['para', ['ul', 'ol', 'paragraph']],
+      ['height', ['height']],
+      ['table', ['table']],
+      ['insert', ['link', 'picture', 'video']],
+      ['view', ['fullscreen', 'codeview', 'help']],
+    ]
   }); 
+
+$("#summernotePhoto").summernote({
+  toolbar: [
+    ['insert', ['picture']]
+  ]
+});
